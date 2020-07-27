0d9112074a5ebfebb6d68f316be3b98a4c1d475e
Unhapppy <951970312@qq.com>
2020-07-27 15:29:51 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/haha/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haha.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
